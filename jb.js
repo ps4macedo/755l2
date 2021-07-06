e689a3f703cca95b1acb42b641cc8bfba6cceadd
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2021-07-05 22:10:00 -0300

    Add files via upload

--- jb.js
+++ jb.js
@@ -1,9 +1,11 @@
 var ropchain_array = new Uint32Array(482118);
 var ropchain = read_ptr_at(addrof(ropchain_array)+0x10);
 
+document.getElementById('myProgress').style.display = 'none';
+document.getElementById('myBar').style.display = 'none';
 var timeleft = 60;
 var downloadTimer = setInterval(function(){
-  document.getElementById("tempo").innerHTML = "Tempo restante: <font style='color:#ee596f'>" + timeleft + "</font> segundos<br><br><font style='color:#ee596f'>Método MSZ_MGS (com alteração)</font>";
+  document.getElementById("tempo").innerHTML = "<br><br><br><br><font style='color:#ee596f'>A G U A R D E !</font><br><br>Tempo restante: <font style='color:#ee596f'>" + timeleft + "</font> segundos<br><br><font style='color:#ee596f'>Método MSZ_MGS (com alteração)</font>";
   timeleft -= 1;
   if(timeleft <= -2){
     clearInterval(downloadTimer);
