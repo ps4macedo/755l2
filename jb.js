3e50104f42417fe45ac7a0e0d43f7d2d64681bb9
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2021-07-05 23:48:25 -0300

    Add files via upload

--- jb.js
+++ jb.js
@@ -2,7 +2,9 @@ var ropchain_array = new Uint32Array(482118);
 var ropchain = read_ptr_at(addrof(ropchain_array)+0x10);
 
 alert("\nNa tentativa de obter uma maior taxa de sucesso, você pode aguardar 2 minutos\nantes de clicar no botão \"OK\". (aqui pode ocorrer kernel panic)");
+contraKP();
 
+function contraKP(){
 var ropchain_offset = 2;
 function set_gadget(val)
 {
@@ -150669,3 +150671,4 @@ var printf_ans = read_mem_as_string(printf_buf, printf_buf_end-printf_buf);
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
 var _ = malloc_nogc.pop();
+}
